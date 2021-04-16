e2035483a50808f016063a4fa28fde725418191a
MohsinAli <mohsinali421@gmail.com>
2021-04-16 07:25:15 +0530

    Final Simple api

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ dotenv.config({path:'./config/config.env'})
 
 const app = express();
 
+app.get('/api/v1/bootcamp',(req,res)=>{
+    res.status(200).json({ success: true, msg: 'All bootcamps'});
+})
+app.post('/api/v1/bootcamp',(req,res)=>{
+    res.status(201).json({ success: true, msg: 'create bootcamps'});
+})
+app.put('/api/v1/bootcamp/:id',(req,res)=>{
+    res.status(201).json({ success: true, msg: 'put bootcamps by id'});
+})
+app.delete('/api/v1/bootcamp/:id',(req,res)=>{
+    res.status(204).json({ success: true, msg: 'delete bootcamps by id'});
+})
+
 const PORT = process.env.PORT||3000;
 
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`));
